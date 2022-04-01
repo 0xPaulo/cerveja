2400143b39755f71a3de1d8df44efb85a745909d
0xPaulo <paulo_czar@outlook.com.br>
2022-04-01 16:06:15 -0300

    fisrt commit

---
+++ src/BeerSong.java
@@ -0,0 +1,33 @@
+
+public class BeerSong {
+	
+	public static void main(String[] args) {
+		
+		int beerNum = 99;
+		String word = "bottles";
+		
+		while (beerNum > 0) {
+			
+			if (beerNum == 1) {
+				word = "bottle"; //1 garrafa
+				
+			}
+		
+			System.out.println(beerNum + " " + word + " " + "of beer on the wall");
+			System.out.println(beerNum + " " + word + " " + "of beer.");
+			System.out.println("Take one down.");
+			System.out.println("pass it around.");
+			beerNum = beerNum - 1;
+			
+			if (beerNum == 1) {
+				word = "bottle"; //1 garrafa	
+			}
+			
+			if (beerNum > 0) {
+				System.out.println(beerNum + " " + word + " " + "of beer on the wall");
+			} else  {
+				System.out.println("No more bottles of beer on the wall");
+			} // fim de else
+		}//fim do loop while
+	}//fim do metodo main
+}//fim da calsse
